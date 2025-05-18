fb01a32e593988de27d9069fb7df598b95d697ce
Manoharibr114 <manoharibrmanu@gmail.com>
2025-05-18 14:10:42 +0530

    Update pb1.java

--- pb1.java
+++ pb1.java
@@ -1,3 +1,5 @@
+/*Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not */
 package prog1;
 import java.util.*;
 public class pb1 {
